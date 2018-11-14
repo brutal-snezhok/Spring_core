fc46569714cd71679232d65de0330bc0626052d6
brutal-snezhok <czyrulik97@mail.ru>
2018-11-14 11:39:11 +0300

    Architecture

---
+++ src/main/java/Client.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Client {
+    private String id;
+    private String fullName;
+
+    public Client(String id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return Objects.equals(getId(), client.getId()) &&
+                Objects.equals(getFullName(), client.getFullName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFullName());
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
