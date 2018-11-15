89644279dc7b4ddb7124fe26cf44a5bbedfc10f6
brutal-snezhok <czyrulik97@mail.ru>
2018-11-15 11:04:45 +0300

    scopes and inner bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,31 +3,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     private Client client;
+
     private EventLogger eventLogger;
 
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+                ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
+
+        Event event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 1");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 2");
+    }
+
     public App(Client client, EventLogger eventLogger) {
+        super();
         this.client = client;
         this.eventLogger = eventLogger;
     }
 
-    public App() {
-
-    }
-
-    public static void main(String[] args) {
-//        Client client = new Client("1", "Dmitry Tsyrulik");
-//        EventLogger eventLogger = new ConsoleEventLogger();
-//        App app = new App(client, eventLogger);
-//        app.logEvent("Some event for user 1");
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-        App app = (App) ctx.getBean("app");
-
-        app.logEvent("Some event for user 1");
-        app.logEvent("Some event for user 2");
-    }
-
-    private void logEvent(String msg){
-        String message = msg.replace(client.getId(), client.getFullName());
-        eventLogger.logEvent(message);
+    private void logEvent(Event event, String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        event.setMsg(message);
+        eventLogger.logEvent(event);
     }
 }
---
+++ src/main/java/TestConsoleEventLogger.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class TestConsoleEventLogger {
+    private static final String MSG = "Message";
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testLogEvent() {
+        ConsoleEventLogger logger = new ConsoleEventLogger();
+        Date date = new Date();
+        Event event = new Event(date, DateFormat.getDateTimeInstance());
+        event.setMsg(MSG);
+
+        logger.logEvent(event);
+
+        Assert.assertTrue(outContent.toString().contains(MSG));
+
+        Assert.assertEquals(event.toString().trim(), outContent.toString().trim());
+    }
+
+}
