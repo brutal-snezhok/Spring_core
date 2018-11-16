fc144ae17d30fa41fece79fc5a5543a70df95d54
brutal-snezhok <czyrulik97@mail.ru>
2018-11-16 22:12:39 +0300

    more opportunities from context

---
+++ src/test/java/TestContext.java
@@ -0,0 +1,19 @@
+import beans.Client;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestContext {
+
+    @Test
+    public void testPropertyPlaceholderSystemOverride() {
+        System.setProperty("client.id", "35");
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        Client client = ctx.getBean(Client.class);
+        ctx.close();
+
+        assertEquals("35", client.getId());
+    }
+}
